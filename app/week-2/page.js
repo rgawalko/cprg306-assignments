7b1a49eba329e643f8fceac6287103150a7a836a
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-09-12 15:16:54 -0600

    fixforpages.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,22 +1,6 @@
-import Link from 'next/link';
+
 import StudentInfo from './student-info';   
 
-export default function Page() {
-  return (
-    <main>
-      <h1>Shopping List</h1>
-      <ul>
-        <li>Apples</li>
-        <li>Bananas</li>
-        <li>Oranges</li>
-      </ul>
-      <p>
-        <Link href="/week-2">Go to Week 2 Page</Link>
-      </p>
-    </main>
-    
-  );
-}
 export default function Week2Page() {
     return (
       <main>
