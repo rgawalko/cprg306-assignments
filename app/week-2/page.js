64ee8d0c749ec405c9a0f8ab3f559e3d925ef3b7
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-09-12 15:06:24 -0600

    Update page.js
    update page

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,19 @@
+import Link from 'next/link';
+
 export default function Page() {
-    return (
-      <main>
-        <h1>Shopping List</h1>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <ul>
+        <li>Apples</li>
+        <li>Bananas</li>
+        <li>Oranges</li>
+      </ul>
+      <p>
+        <Link href="/week-2">
+          <a>Go to Week 2 Page</a>
+        </Link>
+      </p>
+    </main>
+  );
+}
