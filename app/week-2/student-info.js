a2703bfaa0cbb5cd028823fb4c771ad612d4092b
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-09-17 19:26:03 -0600

    Update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -5,11 +5,11 @@ import Link from 'next/link';
 const StudentInfo = () => {
   return (
     <div>
-      <h2>Your Name</h2>
+      <h2>Raymond Gawalko</h2>
       <p>
         Check out my GitHub repository:{" "}
-        <Link href="https://github.com/your-username/your-repository">
-          GitHub Repository
+        <Link href="https://github.com/rgawalko/cprg306-assignments">
+          Rgawalko github repository
         </Link>
       </p>
     </div>
