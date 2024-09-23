9e6afd981c5176c3286d1af5f9af6c59380fdca4
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-09-23 17:58:25 -0600

    Update page.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -5,7 +5,7 @@ import StudentInfo from './student-info';
 export default function Week2Page() {
     return (
       <main>
-        <h1>Week 2</h1>
+        <h1>Shopping List</h1>
         <StudentInfo />
       </main>
     );
