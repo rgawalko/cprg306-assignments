294ebcb2a8ff22b8ed28a8ae9ede8ce0378a6a02
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-09-23 20:01:28 -0600

    Update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function StudentInfo() {
   return (
     <div>
-      <h2>Raymond Gawalko</h2>
+      <h2 className = "bg-slate-600">Raymond Gawalko</h2>
       <p>
         Check out my GitHub repository:{" "}
         <Link href="https://github.com/rgawalko/cprg306-assignments">
