3275091a11606033677e4712ba715b646605af99
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-09-23 21:33:39 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,16 +1,15 @@
 // app/page.js
-import Link from 'next/link';
-
+import Link from "next/link";
 export default function HomePage() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <a>
+      <p>
         <Link href="/week-2">Go to Week 2 Page</Link>
-      </a>
-      <a>
+      </p>
+      <p>
         <Link href="/week-3">Go to Week 3 Page</Link>
-      </a>
+      </p>
     </main>
   );
 }
