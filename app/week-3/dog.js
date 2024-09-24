65fb71b320fbde70278102d038c672c9ced6dfa9
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-09-23 21:45:17 -0600

    Update dog.js

--- app/week-3/dog.js
+++ app/week-3/dog.js
@@ -3,9 +3,9 @@ export default function Dog(props) {
     <section className="m-2 bg-neutral-100">
     <h3 className ="text-xl font-bold
     text-violet-900">{props.name}</h3>
-    <p>Breed: {props.breed}</p>
+    <p><span className="font-bold">Breed:</span> {props.breed}</p>
     <p>Age: {props.age}</p>
     <p>Colour: {props.colour}</p>
-    </section>
+</section>
     );
 }
