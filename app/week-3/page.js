17a16001405c4bfa8407ef142b291b8f5eace156
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-09-23 21:19:16 -0600

    dog component and page changes

---
+++ app/week-3/page.js
@@ -0,0 +1,49 @@
+import Dog from "./dog";
+
+export default function Page() {
+    let dog1 = {
+        name: "Buddy",
+        breed: "Australian Shepherd",
+        age: 11,
+        colour: "brown",
+    };
+
+    let dog2 = {
+        name: "Dingo",
+        breed: "Miniature Schnauzer",
+        age: 7,
+        colour: "black",
+    };
+
+    let dog3 = {
+        name: "Ginger",
+        breed: "Sausage dog",
+        age: 1,
+        colour: "ginger",
+    };
+
+    return (
+        <main className="m-4">
+            <h2 className="text-2xl font-bold">My Dogs</h2>
+            <Dog 
+            name={dog1.name} 
+            breed={dog1.breed} 
+            age={dog1.age} 
+            colour={dog1.colour} 
+            />
+            <Dog>
+            name={dog2.name}
+            breed={dog2.breed}
+            age={dog2.age}
+            colour={dog2.colour}
+            </Dog>
+            <Dog>
+            name={dog3.name}
+            breed={dog3.breed}
+            age={dog3.age}
+            colour={dog3.colour}
+            </Dog>
+        </main>
+    );
+
+}
