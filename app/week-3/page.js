e9ed0ba329edcfe4c46cc756c14f45e4442c9b54
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-09-24 19:55:30 -0600

    Update page.js

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,7 +3,6 @@ import ItemList from "./item-list";
 export default function Page() {
     return (
         <div>
-            <h1>Shopping List</h1>
             <ItemList />
         </div>
     );
