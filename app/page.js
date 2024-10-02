9fa2826105b617b1c4287e72a822dfe2aeb9a0fe
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-10-01 20:02:00 -0600

    Adjustments

--- app/page.js
+++ app/page.js
@@ -5,11 +5,14 @@ export default function HomePage() {
   return (
     <main className="bg-purple-600 min-h-screen m-0 p-0 pt-10 pl-10">
       <h1 className="mb-10">CPRG 306: Web Development 2 - Assignments</h1>
-      <p className=" mb-2">
+      <p>
         <Link href="/week-2">Go to Week 2 Page</Link>
       </p>
       <p>
         <Link href="/week-3">Go to Week 3 Page</Link>
+      </p>
+      <p>
+        <Link href="/week-4">Go to Week 4 Page</Link>
       </p>
     </main>
   );
--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -17,12 +17,23 @@ export default function NewItem() {
     };
         
 
-        return (
-        <div>
-            <p> Quantity: {quantity} </p>
-            <button onClick={increment}> + </button>
-            <button onClick={decrement}> - </button>
-
+    return (
+        <div className="my-5 ml-14 w-48 bg-violet-500 rounded-custom-md flex flex-col items-center justify-center p-4">
+            <p className="font-bold text-gray-100 mb-4"> Quantity: {quantity} </p>
+            <div className="flex space-x-2">
+                <button 
+                    onClick={increment} 
+                    className="bg-blue-700 text-white font-bold py-2 px-4 rounded hover:bg-blue-800 text-2xl  border-blue-900"
+                >
+                    +
+                </button>
+                <button 
+                    onClick={decrement} 
+                    className="bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-600 text-2xl  border-red-700"
+                >
+                    -
+                </button>
+            </div>
         </div>
-        );
+    );
 }
