b2cb557163edd59acc4abb93d8c8d43f620a1ab8
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-10-01 19:27:07 -0600

    correction

---
+++ app/week-4/new-item.js
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState } from "react";
+
+export default function NewItem() {
+    const [quantity, setQuantity] = useState(1);
+    
+    const increment = () => {
+        if (quantity < 20) {
+            setQuantity(quantity + 1);
+        }
+    };
+    const decrement = () => {
+        if (quantity > 1) {
+            setQuantity(quantity - 1);
+        }
+    };
+        
+
+        return (
+        <div>
+            <p> Quantity: {quantity} </p>
+            <button onClick={increment}> + </button>
+            <button onClick={decrement}> - </button>
+
+        </div>
+        );
+}
