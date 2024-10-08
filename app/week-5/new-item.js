7245ad9b1f425972935a2a0e1f2a7bbf51af3e03
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-10-07 21:42:48 -0600

    Update new-item.js

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 
 export default function NewItem() {
     const [quantity, setQuantity] = useState(1);
+    const [name, setName] = useState("");
+    const [category, setCategory] = useState("produce") 
     
     const increment = () => {
         if (quantity < 20) {
