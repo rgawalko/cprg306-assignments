c9c0ab23fc7df55046e60ffe2c45f3eb43ec28d3
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-10-07 21:30:38 -0600

    clean-week-5

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -1,10 +1,10 @@
-import DogForm from "./dog-form";
+ "./dog-form";
 
 export default function Page() {
     return (
         <main className="m-4">
-            <h1 className="text-4xl m-2 bold">Week 5 Interactivity with Forms</h1>
-            <DogForm />
+            
+         
 
         </main>
     )
