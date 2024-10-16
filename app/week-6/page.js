79126611ef3e8d3196f202618f368a191064c8e3
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-10-15 19:50:58 -0600

    week3to6copy

---
+++ app/week-6/page.js
@@ -0,0 +1,9 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+    return (
+        <div className="bg-gray-200 min-h-screen pb-6">
+            <ItemList />
+        </div>
+    ); 
+}
