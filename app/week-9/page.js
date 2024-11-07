d755c86a4529f47ffb1311193258e10b1ae80a59
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-11-06 20:00:34 -0700

    Update page.js

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { useUserAuth } from "./_utils/auth-context";
 import ItemList from "./item-list";
 import NewItem from "./new-item";
 import MealIdeas from "./meal-ideas";
@@ -9,6 +10,15 @@ import itemsData from "./items.json";
 export default function Page() {
     const [items, setItems] = useState(itemsData);
     const [selectedItemName, setSelectedItemName] = useState("");
+    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+    const login = async () => {
+        await gitHubSignIn();
+    };
+
+    const logout = async () => {
+        await firebaseSignOut();
+    };
 
     const handleAddItem = (newItem) => {
         setItems([...items, newItem]);
@@ -26,6 +36,16 @@ export default function Page() {
 
     return (
         <div className="bg-gray-200 min-h-screen flex p-4 space-x-4">
+            <div>
+                {user ? (
+                    <div>
+                        <p>Welcome, {user.displayName}!</p>
+                        <button onClick={logout}>Sign Out</button>
+                    </div>
+                ) : (
+                    <button onClick={login}>Sign In</button>
+                )}
+            </div>
             <div className="flex-1 flex space-x-4">
                 <ItemList items={items} onDelete={handleDelete} onItemSelect={handleItemSelect} />
                 <MealIdeas ingredient={selectedItemName} />
