4c0083626e4e562c15c2bc6cac577178d6cce4d3
rgawalko <166640622+rgawalko@users.noreply.github.com>
2024-11-13 00:28:38 -0700

    Update shopping-list-service.js

--- app/week-10/_services/shopping-list-service.js
+++ app/week-10/_services/shopping-list-service.js
@@ -1,2 +1,28 @@
 import { db } from "../_utils/firebase";
-import { collection, getDocs, addDoc, query } from "firebase/firestore";
+import { collection, getDocs, query, addDoc } from "firebase/firestore";
+
+export const getItems = async (userId) => {
+    try {
+        const itemsCollectionRef = collection(db, `users/${userId}/items`);
+        const itemsSnapshot = await getDocs(itemsCollectionRef);
+
+        const mappedItems = itemsSnapshot.docs.map((itemDoc) => ({
+            id: itemDoc.id,
+            ...itemDoc.data(),
+        }));
+
+        return mappedItems;
+    } catch (fetchItemsError) {
+        console.error("Error fetching items: ", fetchItemsError);
+    }
+};
+
+export const addItem = async (userId, item) => {
+    try {
+        const itemsCollectionRef = collection(db, `users/${userId}/items`);
+        const docRef = await addDoc(itemsCollectionRef, item);
+        return docRef.id;
+    } catch (error) {
+        console.error("Error adding item: ", error);
+    }
+};
